ebc73122abec866fcfd3d7a02ecba1c3fc218760
Chatchawan Kotarasu <IamMitsuo@users.noreply.github.com>
2017-09-13 00:08:14 +0700

    [Fixed] writePosting
    writePosting(), i think, is complete

--- src/VBIndex.java
+++ src/VBIndex.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.Vector;
 
 public class VBIndex implements BaseIndex {
-	
+
 	@Override
 	public PostingList readPosting(FileChannel fc) {
 		// TODO Auto-generated method stub
@@ -23,18 +23,15 @@ public class VBIndex implements BaseIndex {
 		List<Integer> intStream = new Vector<Integer>();
 		intStream.add(p.getTermId()); // add first termId
 		intStream.addAll(docGap(p.getList())); // extend intStream with postings in term of doc-gap
-		System.out.println(intStream);
 		List<Byte> byteStream = VBEncode(intStream); // encode intStream into byteStream via VBEncode algorithm
-		System.out.println(byteStream);
 		/*
 		 * Write byteStream into fc
 		 */
-		
+
 		ByteBuffer byteBuffer = ByteBuffer.allocate(byteStream.size());
 		byteBuffer.clear();
-		for(Byte b : byteStream) {
+		for (Byte b : byteStream)
 			byteBuffer.put(b);
-		}
 		byteBuffer.flip();
 		try {
 			fc.write(byteBuffer);
@@ -43,21 +40,22 @@ public class VBIndex implements BaseIndex {
 			e.printStackTrace();
 		}
 	}
-	
+
 	private static List<Byte> VBEncode(List<Integer> numbers) {
 		List<Byte> byteStream = new ArrayList<Byte>();
-		for(Integer number:numbers) {
+		for (Integer number : numbers) {
 			List<Byte> bytes = VBEncodeNumber(number);
 			byteStream.addAll(bytes);
 		}
 		return byteStream;
 	}
-	
+
 	private static List<Byte> VBEncodeNumber(int number) {
 		LinkedList<Byte> bytes = new LinkedList<Byte>();
-		while(true) {
-			bytes.addFirst((byte)(number % 128));
-			if(number < 128) break;
+		while (true) {
+			bytes.addFirst((byte) (number % 128));
+			if (number < 128)
+				break;
 			number /= 128;
 		}
 		byte b = (byte) (bytes.getLast() + (byte) 128);
@@ -65,12 +63,10 @@ public class VBIndex implements BaseIndex {
 		bytes.addLast(b);
 		return bytes;
 	}
-	
+
 	/**
-	 * change in-order list to be gap list
-	 * for example
-	 * docIds 824 829 215406
-	 * gap 824 5 214577
+	 * change in-order list to be gap list for example docIds 824 829 215406 gap 824
+	 * 5 214577
 	 * 
 	 * @param postings
 	 * @return list
@@ -78,19 +74,21 @@ public class VBIndex implements BaseIndex {
 	private static List<Integer> docGap(List<Integer> postings) {
 		Vector<Integer> list = new Vector<Integer>();
 		list.addElement(postings.get(0));
-		for(int i = 1; i < postings.size(); i++)
-			list.addElement(postings.get(i) - postings.get(i-1));
+		for (int i = 1; i < postings.size(); i++)
+			list.addElement(postings.get(i) - postings.get(i - 1));
 		return list;
 	}
-	
+
 	/**
 	 * To test the correctness of writePosting
 	 * 
 	 * @param args
-	 * @throws FileNotFoundException 
+	 * @throws FileNotFoundException
 	 */
 	public static void main(String[] args) throws FileNotFoundException {
 		File file = new File("./index/small/corpus2.index");
+		file.delete();
+		file = new File("./index/small/corpus2.index");
 		RandomAccessFile raf = new RandomAccessFile(file, "rw");
 		VBIndex index = new VBIndex();
 		List<Integer> l = new ArrayList<Integer>();
