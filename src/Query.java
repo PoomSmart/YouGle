e5269bb453d4b742981937ff66b85031a78f5dcf
Thatchapon Unprasert <poomst@hotmail.com>
2017-09-21 08:14:21 +0700

    Minor.

--- src/Query.java
+++ src/Query.java
@@ -13,7 +13,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.RandomAccessFile;
 import java.nio.channels.FileChannel;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.Iterator;
