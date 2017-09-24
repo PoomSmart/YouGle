d6f9d2d038315b1553c1c2a1529ab6441510db2d
Chatchawan Kotarasu <IamMitsuo@users.noreply.github.com>
2017-09-24 16:40:30 +0700

    Comment for VBIndex

--- src/VBIndex.java
+++ src/VBIndex.java
@@ -16,33 +16,40 @@ import java.util.Vector;
 
 public class VBIndex implements BaseIndex {
 
+	/**
+	 * This method is used to return a PostingList which is read from the FileChannel fc.
+	 * <br>It is used Variable Byte decoding, so it reads as ByteStream.
+	 * @param fc a FileChannel that connects to a file (index file)
+	 * @return <b>PostingList</b> a posting list contains termId and a list of docIds
+	 * 
+	 */
 	@Override
 	public PostingList readPosting(FileChannel fc) {
 		try {
-			ByteBuffer buffer = ByteBuffer.allocate(1);
-			Vector<Integer> numbers = new Vector<Integer>();
+			ByteBuffer buffer = ByteBuffer.allocate(1); // TODO: possible overkill
+			Vector<Integer> numbers = new Vector<Integer>(); // To store numbers decoded from ByteStream
 			int n = 0, docFreq = 0;
 			// Start changing from gaps to real numbers, stored in a list
 			while (fc.read(buffer) != -1) {
 				buffer.rewind();
-				Byte b = buffer.get();
-				if ((b & 0xff) < 128) {
+				Byte b = buffer.get(); // Get a byte, b, from ByteStream
+				if ((b & 0xff) < 128) { // Check if the first bit of b is 0 which means not ending for an encoded byte
 					n = 128 * n + b;
-				} else {
-					n = 128 * n + ((b & 0xff) - 128);
-					numbers.add(n);
+				} else { // If the first bit of b is 1 meaning ending for an encoded byte
+					n = 128 * n + ((b & 0xff) - 128); // Extract the first bit of b out and calculate a number
+					numbers.add(n); // Store a decoded number to a vector
 					n = 0;
-					if (numbers.size() == 2)
+					if (numbers.size() == 2) // Check if it decodes document frequency
 						docFreq = numbers.lastElement();
-					if (numbers.size() - 2 == docFreq)
+					if (numbers.size() - 2 == docFreq) // Check if it is the last element of {docId}
 						break;
 				}
 				buffer.clear();
 			}
 			if (!numbers.isEmpty()) {
 				numbers.trimToSize();
-				List<Integer> postings = docGap(numbers.subList(2, numbers.size()), true);
-				return new PostingList(numbers.get(0), postings);
+				List<Integer> postings = docGap(numbers.subList(2, numbers.size()), true); // Convert a sequence of docId in term of docGap to a normal sequence
+				return new PostingList(numbers.get(0), postings); // Return PostingList
 			}
 			return null;
 		} catch (IOException e) {
@@ -51,14 +58,22 @@ public class VBIndex implements BaseIndex {
 
 	}
 
+	/**
+	 * This method is used to write a PostingList p to a FileChannel fc.
+	 * <br>It is used Variable Byte encoding with p before writing to fc.
+	 * @param fc a FileChannel that connects to a file (index file)
+	 * @param p a PostingList written to fc
+	 */
 	@Override
 	public void writePosting(FileChannel fc, PostingList p) {
-		// We basically encode every integer before writing to file
-		List<Byte> encodedTermId = VBEncodeNumber(p.getTermId());
-		List<Byte> encodedDocFreq = VBEncodeNumber(p.getList().size());
-		List<Byte> encodedDocIds = VBEncode(docGap(p.getList(), false));
+		List<Byte> encodedTermId = VBEncodeNumber(p.getTermId()); // Encoding a termID with VBEncoding
+		List<Byte> encodedDocFreq = VBEncodeNumber(p.getList().size()); // Encoding a docFreq with VBEncoding
+		List<Byte> encodedDocIds = VBEncode(docGap(p.getList(), false)); // Convert a sequence of docIds to be a sequence of docGap and then VBEncoding it
+
 		ByteBuffer byteBuffer = ByteBuffer
-				.allocate(encodedTermId.size() + encodedDocFreq.size() + encodedDocIds.size());
+				.allocate(encodedTermId.size() + encodedDocFreq.size() + encodedDocIds.size()); // Allocate the buffer size
+		
+		/*Put all encoded numbers into ByteBuffer*/
 		for (Byte b : encodedTermId)
 			byteBuffer.put(b);
 		for (Byte b : encodedDocFreq)
@@ -74,36 +89,35 @@ public class VBIndex implements BaseIndex {
 	}
 
 	/**
-	 * Encode the given list of document gaps
-	 * 
-	 * @param numbers
-	 * @return
+	 * This method is used to encode a sequence of numbers with Variable Byte encoding algorithm
+	 * @param numbers a sequence of numbers
+	 * @return ByteStream
 	 */
 	private static List<Byte> VBEncode(List<Integer> numbers) {
 		Vector<Byte> byteStream = new Vector<Byte>();
 		for (Integer number : numbers) {
-			List<Byte> bytes = VBEncodeNumber(number);
-			byteStream.addAll(bytes);
+			List<Byte> bytes = VBEncodeNumber(number); // Encode each number
+			byteStream.addAll(bytes); // Append the new byteStream to the old ByteStream
 		}
 		byteStream.trimToSize();
 		return byteStream;
 	}
 
 	/**
-	 * Encode a number using variable byte method
-	 * 
+
+	 * This method is used to encode a number with Variable Byte encoding algorithm
 	 * @param number
 	 * @return
 	 */
 	private static List<Byte> VBEncodeNumber(int number) {
 		LinkedList<Byte> bytes = new LinkedList<Byte>();
 		while (true) {
-			bytes.addFirst((byte) (number % 128));
+			bytes.addFirst((byte) (number % 128)); // Add into the front of the list
 			if (number < 128)
 				break;
 			number /= 128;
 		}
-		byte b = (byte) (bytes.getLast() + (byte) 128);
+		byte b = (byte) (bytes.getLast() + (byte) 128); // The first bit of the last byte change to 1 for ending bit.
 		bytes.removeLast();
 		bytes.addLast(b);
 		return bytes;
